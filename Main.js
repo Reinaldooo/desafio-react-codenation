d7bf72dc4aa232ac87c30cd562824e661a7d0427
Reinaldo Trindade <rewifetri@gmail.com>
2020-03-28 12:25:03 -0300

    handleJSON function to decipher and update answer.json

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const axios = require("axios");
+let sha1 = require("js-sha1");
 
 const storeJSON = (data, path) => {
   try {
@@ -9,53 +10,57 @@ const storeJSON = (data, path) => {
   }
 };
 
-const loadJSON = (path) => {
-  try {    
-    return JSON.parse(fs.readFileSync(path, 'utf8'))
+const loadJSON = path => {
+  try {
+    return JSON.parse(fs.readFileSync(path, "utf8"));
   } catch (err) {
-    console.error(err)
-    return false
+    console.error(err);
+    return false;
   }
-}
-
-const decipher = ({ cifrado }) => {
-  cifrado.toLowerCase()
-  let decifrado = ""
-  for (var i = 0; i < cifrado.length; i++) {
-    let charCode = cifrado.charCodeAt(i)
-    if (charCode > 96 && charCode < 123) {
-      charCode -= 10
-      if (charCode < 97) {
-        charCode += 26
-      }
-      decifrado += String.fromCharCode(charCode)
-      continue
-    }
-    decifrado += cifrado[i]
-  }
-  return decifrado
 };
 
-// axios
-//   .get(
-//     "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=2706026471a1e62a3882211060985bafe80b72f0"
-//   )
-//   .then(({ data }) => {
-//     // handle success
-// if( k > 26) {
-//   k = k % 26;
-// }
-//     storeJSON(data, "./answer.json");
-//     loadJSON("./answer.json");
-//   })
-//   .catch((error) => {
-//     // handle error
-//     console.log(error);
-//   })
-//   // .finally(function() {
-//   //   // always executed
-//   // });
+const decipher = ({ cifrado, numero_casas }) => {
+  cifrado.toLowerCase();
+  let decifrado = "";
+  for (var i = 0; i < cifrado.length; i++) {
+    let charCode = cifrado.charCodeAt(i);
+    if (charCode > 96 && charCode < 123) {
+      charCode -= numero_casas;
+      if (charCode < 97) {
+        charCode += 26;
+      }
+      decifrado += String.fromCharCode(charCode);
+      continue;
+    }
+    decifrado += cifrado[i];
+  }
+  return decifrado;
+};
 
-let text = loadJSON("./answer.json");
+const handleJSON = async () => {
+  return await axios
+    .get(
+      "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=2706026471a1e62a3882211060985bafe80b72f0"
+    )
+    .then(({ data }) => {
+      if (data.numero_casas > 26) {
+        data.numero_casas = data.numero_casas % 26;
+      }
+      storeJSON(data, "./answer.json");
+      console.log("ok");
+    })
+    .then(() => {
+      let answer = loadJSON("./answer.json");
+      let decifrado = decipher(answer);
+      answer.decifrado = decifrado;
+      answer.resumo_criptografico = sha1(decifrado);
+      storeJSON(answer, "./answer.json");
+      return answer;
+    })
+    .catch(error => {
+      console.log(error);
+      return false;
+    });
+};
 
-decipher(text)
+handleJSON()
